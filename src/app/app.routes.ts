f8a8c66a9b54945324d14674b9d8f7b444098b27
Jorge Fernández Marín <jofmar00@gmail.com>
2025-09-09 20:59:26 +0200

    idk

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { Contact } from './components/contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component: Home},
+    {path: 'contact', component: Contact},
+    {path: '**', redirectTo: 'home'}
+];
